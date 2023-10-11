2b76064caf50664f152a0e141db743c588001d93
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-10-10 22:39:04 -0300

    Initial commit

---
+++ src/test/java/br/com/toplibrary/ToplibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toplibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToplibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
